c473efc39958b7ec66fbdf1139ba2874331d7064
rsascha <sascha.rose@gmail.com>
2020-05-13 19:35:05 +0200

    Do some templating for development tools

--- development-tools/k8s/build.sh
+++ development-tools/k8s/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 VERSION=$(cat version) && ((VERSION=VERSION+1)) && echo $VERSION > version
-IMAGE=localhost:5000/development-tools:$VERSION
+export VERSION=$VERSION
 
+SOURCE_FILE="templates/deployment.yaml"
+TARGET_FILE="manifest-files/deployment.yaml"
+echo "# GENERATED FROM: ${SOURCE_FILE}" > $TARGET_FILE
+cat $SOURCE_FILE | envsubst >> $TARGET_FILE
+
+SOURCE_FILE="templates/index.html"
+TARGET_FILE="../src/index.html"
+echo "<!-- GENERATED FROM: ${SOURCE_FILE} -->" > $TARGET_FILE
+cat $SOURCE_FILE | envsubst >> $TARGET_FILE
+
+IMAGE=localhost:5000/development-tools:$VERSION
 cd .. && docker build --tag $IMAGE . && cd -
 
 docker push $IMAGE
-
-export VERSION=$VERSION
-envsubst < templates/deployment.yaml > manifest-files/deployment.yaml
