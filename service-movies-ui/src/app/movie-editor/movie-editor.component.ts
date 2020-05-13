af6d6e54cf13692f609b74c9b7e06b153672e9ab
rsascha <sascha.rose@gmail.com>
2020-05-13 11:38:56 +0200

    Backup during configuration

--- service-movies-ui/src/app/movie-editor/movie-editor.component.ts
+++ service-movies-ui/src/app/movie-editor/movie-editor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Movie, DefaultService } from 'src/api-movies';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-movie-editor',
@@ -20,8 +21,8 @@ export class MovieEditorComponent implements OnInit {
     }
 
     getMovies(): Observable<Movie[]> {
-        const serviceMoviesHost = 'localhost';
-        const serviceMoviesPort = '4200';
+        const serviceMoviesHost = environment.serviceMoviesHost;
+        const serviceMoviesPort = environment.serviceMoviesPort;
         const defaultApi = new DefaultService(this.httpClient, `http://${serviceMoviesHost}:${serviceMoviesPort}`, null);
         return defaultApi.appControllerGetMovies();
     }
